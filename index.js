21b578c01e277f4e552da7254185ebcbc8237bc9
shaheer-alam-bit <s.alam.25144@khi.iba.edu.pk>
2025-06-08 04:09:22 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const { Sequelize, DataTypes } = require('sequelize');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const sequelize = new Sequelize('postgres://postgres:postgres@localhost:5432/todo_db');
+
+const Todo = sequelize.define('Todo', {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+});
+
+sequelize.sync({ alter: true });
+
+app.get('/api/todos', async (req, res) => {
+  const todos = await Todo.findAll();
+  res.json(todos);
+});
+
+app.post('/api/todos', async (req, res) => {
+  const { title, description } = req.body;
+  const todo = await Todo.create({ title, description });
+  res.json(todo);
+});
+
+app.delete('/api/todos/:id', async (req, res) => {
+  const { id } = req.params;
+  await Todo.destroy({ where: { id } });
+  res.json({ message: 'Todo deleted' });
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
